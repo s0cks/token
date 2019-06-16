efa48250755195e2eaf96f931bd454d235bbb73c
Tazz <tasman.vose@gmail.com>
2019-06-16 10:26:36 -0600

    Remove shitty server code

--- include/blockchain.h
+++ include/blockchain.h
@@ -1,8 +1,6 @@
 #ifndef TOKEN_BLOCKCHAIN_H
 #define TOKEN_BLOCKCHAIN_H
 
-#include <uv.h>
-#include <memory>
 #include <map>
 #include "common.h"
 #include "array.h"
@@ -142,54 +140,6 @@ namespace Token{
         bool Load(const std::string& root);
         bool Load(const std::string& root, const std::string& addr, int port);
         bool Save(const std::string& root);
-
-        class Server{
-        private:
-            static const size_t MAX_BUFF_SIZE = 4096;
-
-            uv_loop_t loop_;
-            uv_tcp_t server_;
-            ByteBuffer read_buff_;
-            bool running_;
-            User user_;
-            BlockChain* chain_;
-            std::map<uv_stream_t*, Session*> sessions_;
-
-            void OnNewConnection(uv_stream_t* server, int status);
-            void OnMessageReceived(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buff);
-            void OnMessageSent(uv_write_t* stream, int status);
-            void Send(uv_stream_t* conn, Message* msg);
-
-            void RemoveClient(uv_stream_t* client){
-                //TODO: Implement
-            }
-
-            bool Handle(Session* session, Message* msg);
-
-            Server():
-                    user_("TestUser"),
-                    sessions_(),
-                    running_(false),
-                    server_(),
-                    loop_(),
-                    read_buff_(MAX_BUFF_SIZE){}
-            friend class BlockChain;
-        public:
-            static void AddPeer(const std::string& addr, int port);
-            static int Initialize(int port);
-            static int Initialize(int port, const std::string& paddr, int pport);
-            ~Server(){}
-
-            void Broadcast(uv_stream_t* from, Message* msg);
-
-            ByteBuffer* GetReadBuffer(){
-                return &read_buff_;
-            }
-
-            bool IsRunning() const{
-                return running_;
-            }
-        };
     };
 }
 
