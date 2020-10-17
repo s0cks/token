d8ed6b9f3ff209f8223a3f74f0a94a1c69378bf2
Tazz <tasman.vose@gmail.com>
2020-10-16 22:00:00 -0600

    need to eventually fix the unit tests

--- tests.cc
+++ tests.cc
@@ -1,3 +1,4 @@
+#include <gtest/gtest.h>
 #include "token_tests.h"
 
 int
--- tests/test_test.cc
+++ tests/test_test.cc
@@ -1,7 +1,9 @@
-#include "test_test.h"
+#include <gtest/gtest.h>
+#include <glog/logging.h>
+#include "token.h"
 
 namespace Token{
-    TEST_F(TestTest, test_test){
-        ASSERT_EQ(Token::GetVersion(), "1.0.0");
+    TEST(TestTest, test_test){
+        ASSERT_EQ(Token::GetVersion(), "1.0.1");
     }
 }
