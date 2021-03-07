9b693c13b7541502a45d614da7329b8730eb3fa9
Tazz <tasman.vose@gmail.com>
2021-03-06 18:19:32 -0700

    cleanup WalletManager class

--- src/wallet.cc
+++ src/wallet.cc
@@ -1,10 +1,6 @@
-#include <mutex>
-#include <condition_variable>
-
 #include <leveldb/db.h>
 #include <leveldb/slice.h>
 
-#include "block.h"
 #include "wallet.h"
 #include "configuration.h"
 
@@ -22,19 +18,6 @@ namespace token{
 #define WMGR_LOG(LevelName) \
   LOG(LevelName) << "[WalletManager] "
 
-
-  //TODO: remove locking
-  static std::mutex mutex_;
-  static std::condition_variable cond_;
-  static WalletManager::State state_ = WalletManager::kUninitialized;
-  static WalletManager::Status status_ = WalletManager::kOk;
-  static leveldb::DB* index_ = nullptr;
-
-  static inline leveldb::DB*
-  GetIndex(){
-    return index_;
-  }
-
   static inline std::string
   GetBlockChainHome(){
     return ConfigurationManager::GetString(TOKEN_CONFIGURATION_BLOCKCHAIN_HOME);
@@ -45,59 +28,8 @@ namespace token{
     return GetBlockChainHome() + "/wallet";
   }
 
-  WalletManager::State WalletManager::GetState(){
-    std::lock_guard<std::mutex> guard(mutex_);
-    return state_;
-  }
-
-  void WalletManager::SetState(const State& state){
-    std::unique_lock<std::mutex> lock(mutex_);
-    state_ = state;
-    lock.unlock();
-    cond_.notify_all();
-  }
-
-  WalletManager::Status WalletManager::GetStatus(){
-    std::lock_guard<std::mutex> guard(mutex_);
-    return status_;
-  }
-
-  void WalletManager::SetStatus(const Status& status){
-    std::unique_lock<std::mutex> lock(mutex_);
-    status_ = status;
-    lock.unlock();
-    cond_.notify_all();
-  }
-
-  bool WalletManager::Initialize(){
-    if(IsInitialized()){
-#ifdef TOKEN_DEBUG
-      WMGR_LOG(WARNING) << "cannot re-initialize the wallet manager.";
-#endif//TOKEN_DEBUG
-      return false;
-    }
-
-    WMGR_LOG(INFO) << "initializing....";
-    SetState(WalletManager::kInitializing);
-
-    leveldb::Options options;
-    options.create_if_missing = true;
-    options.comparator = new WalletManager::Comparator();
-    leveldb::Status status;
-    if(!((status = leveldb::DB::Open(options, GetIndexFilename(), &index_)).ok())){
-      WMGR_LOG(ERROR) << "couldn't initialize the index: " << status.ToString();
-      return false;
-    }
-
-#ifdef TOKEN_DEBUG
-    WMGR_LOG(INFO) << "initialized.";
-#endif//TOKEN_DEBUG
-    SetState(WalletManager::kInitialized);
-    return true;
-  }
-
   //TODO: cleanup
-  bool WalletManager::PutWallet(const User& user, const Wallet& wallet){
+  bool WalletManager::PutWallet(const User& user, const Wallet& wallet) const{
     UserKey key(user);
 
     BufferPtr buffer = Buffer::NewInstance(GetBufferSize(wallet));
@@ -118,7 +50,7 @@ namespace token{
   }
 
   //TODO: refactor
-  bool WalletManager::GetWallet(const User& user, Wallet& wallet){
+  bool WalletManager::GetWallet(const User& user, Wallet& wallet) const{
     UserKey key(user);
 
     std::string data;
@@ -131,7 +63,7 @@ namespace token{
   }
 
   //TODO: refactor
-  bool WalletManager::GetWallet(const User& user, Json::Writer& writer){
+  bool WalletManager::GetWallet(const User& user, Json::Writer& writer) const{
     UserKey key(user);
     std::string data;
     leveldb::Status status;
@@ -156,7 +88,7 @@ namespace token{
   }
 
   //TODO: refactor
-  bool WalletManager::RemoveWallet(const User& user){
+  bool WalletManager::RemoveWallet(const User& user) const{
     leveldb::WriteOptions options;
     options.sync = true;
 
@@ -172,7 +104,7 @@ namespace token{
     return true;
   }
 
-  bool WalletManager::HasWallet(const User& user){
+  bool WalletManager::HasWallet(const User& user) const{
     std::string data;
     UserKey key(user);
     return GetIndex()->Get(leveldb::ReadOptions(), key, &data).ok();
@@ -184,12 +116,54 @@ namespace token{
     return GetIndex()->Write(opts, (leveldb::WriteBatch*)&batch);
   }
 
-  int64_t WalletManager::GetNumberOfWallets(){
+  int64_t WalletManager::GetNumberOfWallets() const{
     int64_t count = 0;
     leveldb::Iterator* it = GetIndex()->NewIterator(leveldb::ReadOptions());
     for(it->SeekToFirst(); it->Valid(); it->Next())
       count++;
     delete it;
     return count;
+  }
+
+  class LocalWalletManager : public WalletManager{
+   private:
+    bool Initialize(){
+      if(IsInitialized()){
+#ifdef TOKEN_DEBUG
+        WMGR_LOG(WARNING) << "cannot re-initialize the wallet manager.";
+#endif//TOKEN_DEBUG
+        return false;
+      }
+
+      WMGR_LOG(INFO) << "initializing....";
+      SetState(WalletManager::kInitializingState);
+
+      leveldb::Options options;
+      options.create_if_missing = true;
+      options.comparator = new WalletManager::Comparator();
+      leveldb::Status status;
+      if(!((status = leveldb::DB::Open(options, GetIndexFilename(), &index_)).ok())){
+        WMGR_LOG(ERROR) << "couldn't initialize the index: " << status.ToString();
+        return false;
+      }
+
+#ifdef TOKEN_DEBUG
+      WMGR_LOG(INFO) << "initialized.";
+#endif//TOKEN_DEBUG
+      SetState(WalletManager::kInitializedState);
+      return true;
+    }
+   public:
+    LocalWalletManager():
+      WalletManager(){
+      if(!Initialize())
+        WMGR_LOG(ERROR) << "failed to initialize the wallet manager.";
+    }
+    ~LocalWalletManager() = default;
+  };
+
+  WalletManager* WalletManager::GetInstance(){
+    static LocalWalletManager instance;
+    return &instance;
   }
 }
